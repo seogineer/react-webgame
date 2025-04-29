95d1a597cd771bdb492cfb1f5a18c312f79286f2
seogineer <dgseo8981@gmail.com>
2025-04-29 16:52:41 +0900

    feat: 2-7. @babel/preset-env와 plugins

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -12,10 +13,22 @@ module.exports = {
       test: /\.jsx?$/,
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 5% in KR', 'last 2 chrome versions'],
+            },
+            debug: true,
+          }],
+          '@babel/preset-react'
+        ],
+        plugins: [],
       },
     }],
   },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
