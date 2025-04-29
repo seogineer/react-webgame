17b45528851436a56d1d3ba36f2145fc30099b3b
seogineer <dgseo8981@gmail.com>
2025-04-29 13:33:16 +0900

    feat: 2-4. 모듈 시스템과 웹팩 설정

--- 2.끝말잇기/client.jsx
+++ 2.끝말잇기/client.jsx
@@ -1,2 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+
+const WordRelay = require('./WordRelay');
+
+ReactDom.render(<WordRelay/>, document.querySelector('#root'));
--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'deveolopment', // 실서비스: production
+  devtool: 'eval',
+  // resolve: {
+  //   extensions: ['.js', '.jsx'],
+  // },  // entry.app 파일 확장자를 사용하고 싶지 않는 경우 사용
+
+  entry: {
+    app: ['./client.jsx'],
+  },  // 입력
+  output: {
+    path: path.join(__dirname, 'dist'), // /Users/seogineer/Documents/workspace/react-webgame/2.끝말잇기/dist
+    filename: 'app.js',
+  },  // 출력
 };
