fee7029d084801e8f3507eec04201bf5ca5cfd60
seogineer <dgseo8981@gmail.com>
2025-05-03 01:12:05 +0900

    feat: 6-2. setTimeout 여러 번 사용하기

--- 6.로또추첨기/Lotto.jsx
+++ 6.로또추첨기/Lotto.jsx
@@ -20,16 +20,50 @@ class Lotto extends Component {
     redo: false,
   };
 
+  timeouts = [];
+
+  componentDidMount() {
+    const { winNumbers } = this.state;
+    for (let i = 0; i < this.state.winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
+
+  onClickRedo() {
+
+  }
+
   render() {
-    <>
-      <div>당첨 숫자</div>
-      <div id="결과창">
-        {winBalls.map((v) => <Ball key={v} number={v} />)}
-      </div>
-      <div>보너스!</div>
-      {bonus && <Ball number={bonus} onClick={onClickRedo} />}
-      {redo && <button onClick={onClickRedo}>한 번 더!</button>}
-    </>
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => <Ball key={v} number={v} />)}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
+      </>
+    );
   }
 }
 
