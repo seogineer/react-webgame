ca47240f142ab44fc2a354a06e611346236f1ca1
seogineer <dgseo8981@gmail.com>
2025-05-02 21:34:35 +0900

    feat: 5-1. 리액트 라이프사이클 소개

---
+++ 5.가위바위보/RSP.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+// 클래스의 실행 순서: constructor -> render -> ref -> componentDidMount
+// -> (setState/props 바뀔 때) -> shouldComponentUpdate(true) -> render -> componentDidUpdate
+// 부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
+
+class RSP extends Component {
+  state = {
+    result: ``,
+    imgCoord: 0,
+    score: 0,
+  };
+
+  componentDidMount() { // 컴포넌트가 첫 렌더링된 후
+
+  }
+
+  componentDidUpdate() {  // 리렌더링
+
+  }
+
+  componentWillUnmount() {  // 컴포넌트가 제거되기 직전
+
+  }
+
+  render() {
+    const { result, score, imgCoord } = this.state;
+    return (
+      <>
+        <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
+        <div>
+          <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
+          <button id="scissor" className="btn" onClick={onClickBtn('가위')}>가위</button>
+          <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
+        </div>
+        <div>{result}</div>
+        <div>현재 {score}점</div>
+      </>
+    );
+  }
+}
+
+export default RSP;
