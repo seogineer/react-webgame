64eec3dc96cb1db72d8dbc329c211da0ac734256
seogineer <dgseo8981@gmail.com>
2025-05-03 19:30:43 +0900

    feat: 7-3. action 만들어 dispatch 하기

--- 7.틱택토/TicTacToe.jsx
+++ 7.틱택토/TicTacToe.jsx
@@ -3,11 +3,17 @@ import Table from './Table';
 
 const initialState = {
   winner: '',
-  turn: '0',
-  tableData: [['', '', ''], ['', '', ''], ['', '', '']],
+  turn: 'O',
+  tableData: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ],
 };
 
-const SET_WINNER = 'SET_WINNER';
+export const SET_WINNER = 'SET_WINNER';
+export const CLICK_CELL = 'CLICK_CELL';
+export const CHANGE_TURN = 'CHANGE_TURN';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -16,8 +22,20 @@ const reducer = (state, action) => {
       return {
         ...state,
         winner: action.winner,
-      }
-
+      };
+    case CLICK_CELL:
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...tableData[action.row]];
+      tableData[action.row][action.cell] = state.turn;
+      return {
+        ...state,
+        tableData,
+      };
+    case CHANGE_TURN:
+      return {
+        ...state,
+        turn: state.turn === 'O' ? 'X' : 'O',
+      };
   }
 };
 
@@ -30,12 +48,12 @@ const TicTacToe = () => {
   // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
 
   const onClickTable = useCallback(() => {
-    dispatch({ type: 'SET_WINNER', winner: '0' });
+    dispatch({ type: 'SET_WINNER', winner: 'O' });
   }, []);
 
   return (
     <>
-      <Table onClick={onClickTable} tableData={state.tableData} />
+      <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch} />
       {winner && <div>{winner}님의 승리</div>}
     </>
   );
